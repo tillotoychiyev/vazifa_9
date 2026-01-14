14cca67e3526569a956f335307f648f3cb2ce929
tillotoychiyev <tillotoychiyev5@gmail.com>
2026-01-14 22:19:12 +0500

    2-topshiriq

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-print("________________________________")
-print("_________1-topshiriq____________")
+print("__________________________________")
+print("_________ 1-topshiriq ____________")
+
 numbers = [1,2,3,4]
 res = list(map(lambda value: value * 3, numbers))
 print(res)
+
+print("__________________________________")
+print("_________ 2-topshiriq ____________")
+
+numbers = [1,2,3,4]
+k_num = list(map(lambda value: value ** 2, numbers))
+print(k_num)
+
